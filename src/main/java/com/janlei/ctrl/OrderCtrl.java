3e7ebdbabf531d5cfdfe41b7f051e56bfe3a57b0
Janlei-CN <janlei4597@gmail.com>
2020-08-12 10:15:01 +0800

    refactor(controller): add the requestMapping

--- src/main/java/com/janlei/ctrl/OrderCtrl.java
+++ src/main/java/com/janlei/ctrl/OrderCtrl.java
@@ -3,9 +3,11 @@ package com.janlei.ctrl;
 import com.janlei.annotation.Token;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/order")
 @Slf4j
 public class OrderCtrl {
 
