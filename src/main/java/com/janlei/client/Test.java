fec2c2132420f5c1ff053d5dff30bdf3834589c5
Janlei-CN <janlei4597@gmail.com>
2020-08-19 21:46:32 +0800

    Revert "refactor(SPI): add the case of testing"
    This reverts commit 40187fe9

--- src/main/java/com/janlei/client/Test.java
+++ src/main/java/com/janlei/client/Test.java
@@ -1,11 +1,9 @@
-package com.janlei.spi.client;
+package com.janlei.client;
 
 import com.janlei.spi.ISpiStu;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.ServiceLoader;
 
-@Slf4j(topic = "c.Test")
 public class Test {
 
     public static void main(String[] args) {
@@ -13,7 +11,5 @@ public class Test {
         for (ISpiStu impl : load) {
             impl.parse();
         }
-
-        log.info("test");
     }
 }
