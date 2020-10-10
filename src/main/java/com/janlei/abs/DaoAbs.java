2253cd5e416c1ed6d261997e649901ebcf3c7d9b
Janlei-CN <janlei4597@gmail.com>
2020-10-10 13:32:26 +0800

    feat(DaoAbs): add the test case and abstract class

---
+++ src/main/java/com/janlei/abs/DaoAbs.java
@@ -0,0 +1,11 @@
+package com.janlei.abs;
+
+/**
+ * @author admin
+ */
+public abstract class DaoAbs {
+    /**
+     * say something
+     */
+    public abstract void say();
+}
---
+++ src/test/java/com/janlei/abs/DaoAbsTest.java
@@ -0,0 +1,26 @@
+package com.janlei.abs;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DaoAbsTest {
+
+    @Qualifier("mysqlDaoImpl")
+    @Autowired
+    private DaoAbs daoAbs;
+
+    @Test
+    public void test_check_who_say(){
+        daoAbs.say();
+    }
+}
